db4afc974cfb1784fb99a3b89949e2cb41e8cfcf
cm-jones <cm-jones@users.noreply.github.com>
2024-07-04 15:46:47 +0000

    deploy: 8b025fe2f260a94ec396bfbafb21ae55e27bad90

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['log_104',['log',['../classcramer_1_1Matrix.html#a6dce72075621b1ed3c58348aac4a3dc2',1,'cramer::Matrix::log()'],['../classcramer_1_1Vector.html#a0d9d2016ecf055d52606d296cd2a1b93',1,'cramer::Vector::log()']]],
+  ['log_104',['log',['../classcramer_1_1Vector.html#a0d9d2016ecf055d52606d296cd2a1b93',1,'cramer::Vector']]],
   ['lu_105',['lu',['../classcramer_1_1Matrix.html#aff64bf587f72f585e32efda9d4c8c964',1,'cramer::Matrix']]]
 ];
